b859d8d389be8595452ec95bc7fa544f6c653a27
Matthew Moczynski <103980603+mmoczynski@users.noreply.github.com>
2024-09-09 13:01:15 -0500

    Merging polygon creator and definer

--- app/js/src/polygonCreator.js
+++ app/js/src/polygonCreator.js
@@ -1,15 +1,73 @@
-import PolygonDefiner from "./polygonDefiner.js";
+import EventHandler from "./eventHandler.js";
 
 /**
  * @constructor
  * @param {HTMLElement} canvas
- * @extends PolygonDefiner
  * 
  */
 
  function PolygonCreator(canvas) {
 
-	PolygonDefiner.apply(this,arguments);
+	let self = this;
+
+	this.delta = {
+		x: 0,
+		y: 0
+	}
+
+	let polygonDefiner = this;
+
+	/**
+	 * Vertices 
+	 * @type {Vector[]}
+	 */
+
+	this.vertices = [];
+
+	/**
+	 * Target element
+	 * @type {HTMLElement}
+	 */
+
+	this.canvas = canvas;
+
+	/**
+	 * String defining trigger for HTML Event
+	 * @type {String}
+	 */
+
+	this.firstEventStr = "click";
+	this.secondEventStr = "dblclick";
+
+	this.eventArrays = {
+		definePolygon: [],
+		pushVector: [],
+		disable: [],
+		enable: []
+	};
+
+	this.addVectorByMouseEvent = function(event) {
+		let v = {
+			x: event.offsetX - self.delta.x,
+			y: event.offsetY - self.delta.y,
+		};
+
+		polygonDefiner.vertices.push(v);
+
+		polygonDefiner.callEvent("pushVector", {
+			vector: v,
+		});
+	}
+
+	this.definePolygonByMouseEvent = function () {
+		polygonDefiner.callEvent("definePolygon", {
+			vertices: polygonDefiner.vertices,
+		});
+
+		polygonDefiner.vertices = [];
+	}
+
+    Object.assign(this,EventHandler);
 
 	// this.on("pushVector",function(event){
 	// 	alert("Vector: " + JSON.stringify(event));
@@ -21,6 +79,29 @@ import PolygonDefiner from "./polygonDefiner.js";
 
 }
 
-Object.assign(PolygonCreator.prototype,PolygonDefiner.prototype);
+PolygonCreator.prototype.enable = function() {
+
+	this.canvas.addEventListener(this.firstEventStr, this.addVectorByMouseEvent);
+	this.canvas.addEventListener(this.secondEventStr, this.definePolygonByMouseEvent);
+	
+	this.callEvent("enable", {
+		targetElement: this.canvas,
+		firstEventStr: this.firstEventStr,
+		secondEventStr: this.secondEventStr
+	});
+}
+
+PolygonCreator.prototype.disable = function() {
+	
+	this.canvas.removeEventListener(this.firstEventStr, this.addVectorByMouseEvent);
+	this.canvas.removeEventListener(this.secondEventStr, this.definePolygonByMouseEvent);
+
+	this.callEvent("disable", {
+		targetElement: this.canvas,
+		firstEventStr: this.firstEventStr,
+		secondEventStr: this.secondEventStr
+	});
+
+}
 
 export default PolygonCreator;
