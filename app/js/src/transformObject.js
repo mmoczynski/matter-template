1fac6d53415cf21edc14dd582ade71895fba462e
Matthew Moczynski <103980603+mmoczynski@users.noreply.github.com>
2025-02-12 21:11:38 -0600

    Fixing bug in which object that is transformed does not get put in the right place in simulation.

--- app/js/src/transformObject.js
+++ app/js/src/transformObject.js
@@ -1,16 +1,9 @@
 export function transformObject(object, vector) {
-    
-    // Used for transforming vectors, circles, and any other thing that is defined with a vector
 
-    if( Array.isArray(object.vertexSets) ) {
-        transformObject(object.vertexSets, vector);
-    }
-
-    // Transform object that is defined by array by transforming elements of array
-
-    else if(Array.isArray(object)) {
-        for(var i = 0; i < object.length; i++) {
-            transformObject(object[i], vector);
+    if(object.shape === "vertices") {
+        for(var i = 0; i < object.vertexSets.length; i++) {
+            object.vertexSets[i].x += vector.x
+            object.vertexSets[i].y += vector.y
         }
     }
 
@@ -21,11 +14,9 @@ export function transformObject(object, vector) {
         object.y += vector.y;
     }
 
-    // Throw error for unsupported object
-
-    else {
-        throw "Unsupported object";
-    }
+    //else {
+        //throw "Unsupported object";
+    //}
 
 
 }
